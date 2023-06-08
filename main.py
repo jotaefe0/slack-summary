6386fc7056b06d5df1d44689956168662d5d5f73
juanfranrau <114007242+juanfranrau@users.noreply.github.com>
2023-06-08 14:49:58 -0300

    channel id

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = FastAPI()
 
 class SlashCommandPayload(BaseModel):
     channel_id: str
+   
 
 def send_summary():
     summary = utils.create_summary()
@@ -15,7 +16,8 @@ def send_summary():
 @app.post("/summary")
 async def trigger_endpoint(background_tasks: BackgroundTasks, payload: SlashCommandPayload):
     #Slack backslash expects a response in a 3 sec so we process on background
-    background_tasks.add_task(send_summary)
+    # background_tasks.add_task(send_summary)
+    
     channel_id = payload.channel_id
     return f"this was sent from {channel_id}"
     return Response(status_code=200)
